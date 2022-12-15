72d88b03bd34d25fe781763de1809aba97463d54
BeakSungHo <tjdgh9023@naver.com>
2022-12-15 20:17:51 +0900

    tt

--- src/GG-Project_Server/ggmap/ggmap.controller.ts
+++ src/GG-Project_Server/ggmap/ggmap.controller.ts
@@ -14,7 +14,7 @@ export class GgmapController {
     getMarkerInfo(@Body() getID):Promise<MarkerInfo>{
       return this.GgmapService.getMarkerInfo(getID);
     }
-    //124134431434143143431413
+    //
     @Get('/test')
     getAllMarkerInfo():Promise<MarkerInfo[]>{
       return this.GgmapService.getAllMarkerInfo();
